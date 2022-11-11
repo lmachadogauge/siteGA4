69796f1cedcf2db7aaa8b51a514f7c160717131e
Lucas Machado <lucasmachado@Lucass-MacBook-Pro.local>
2022-11-11 11:46:27 -0300

    button click

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,35 @@
-function Home(){
-    return <div>Home</div>
+import { useState } from 'react';
+
+function Home() {
+    return <div>
+        <h1>Página do seu Lucas </h1>
+        <h2>cic >> adm </h2>
+        <Contador />
+        
+        </div>
+}
+
+function Contador(){
+    const[contador,setContador] = useState(1);
+
+    function adicionarContador(){
+        setContador(contador + 1);
+    }
+    function resetarContador(){
+        setContador(0);
+    }
+    function subtrairContador(){
+        setContador(contador - 1)
+    }
+    return(
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+            <button onClick={subtrairContador}>Subtrair</button>
+            <button onClick={resetarContador}>Resetar</button>
+        </div>
+    )    
+
 }
 
 export default Home
